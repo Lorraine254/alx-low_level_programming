6f8dffc7621b08b4c7fff43970cceb9d8db8b0cd
Lorraine437 <lorraine.atsulu@gmail.com>
2022-09-14 05:21:21 -0700

    Function that prints the alphabet in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H_
-#define MAINN_H_
+#define MAIN_H_
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,4 +14,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* _HOLBERTON_H_ */
+#endif /* _MAIN_H_ */
