4b490091061b66ce518c4bbc896062ff54680984
Lorraine437 <lorraine.atsulu@gmail.com>
2022-09-28 03:21:20 -0700

    Header file and README file

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# RECURSION
+## -Is a method where solution to a problem depends on solutions to smaller instances of the same problem.
+## -Use cases: Factorial of a number,GEnerating fibonacci series.
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * File:main.h
+ * Auth: Lorraine
+ * description: header file containing prototypes for all functions
+ * used in the0x08-Recursion
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
